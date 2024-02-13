ad7a8dec91da967f40540ce7a7e2d714fbd405bf
aman <amankhanna224466@gmail.com>
2024-02-13 16:58:24 +0530

    additions

--- attributes.py
+++ attributes.py
@@ -6,24 +6,26 @@ arr:np.ndarray = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 o:tuple = np.shape(arr)
 # or
 arr.shape
-print(o)  # returns a tuple of (rows, column)
+# print(o)  # returns a tuple of (rows, column)
+
+# reshape
+a = np.reshape(arr, [4,3])
+print(a)
 
 # size --> total no of elements
 s:int = np.size(arr)
-print(s)
 
 # ndim --> no of dimensions
 d:int = np.ndim(arr)
-print(d)
 
 # len --> no of nested elements
 d:int = len(arr)
 
 # dtype --> which datatype is used in the matrix
 dt = arr.dtype
-print(dt)
+# print(dt)
 
 # astype --> converting the datatype
 # if i convert float to int, it will move to LOWER integer
 at = arr.astype(float)
-print(at)
+# print(at)
--- intro.py
+++ intro.py
@@ -5,15 +5,38 @@ import numpy as np
 a1: np.ndarray = np.array([10, 20, 30])
 a2: np.ndarray = np.array([2, 3, 4])
 
-# addition (adds corresponding elements)
-add: np.ndarray = a1 + a2
+# creates array with 1 row 5 columns with elements as 1
+b: np.ndarray = np.ones(5)  # elements will be float
+b1: np.ndarray = np.ones(5, dtype=int)  # elements will be int
 
-# multiply (multiplies corresponding elements)
-prod: np.ndarray = a1 * a2
-multiply_everyone_with_4: np.ndarray = a1 * 4
+# create 2d
+c: np.ndarray = np.ones([3,3], dtype=int)
+
+# create zero matrix
+z: np.ndarray = np.zeros([3, 3], dtype=int)
+
+# creating array through a range
+d = np.arange(3)    #creates an array from 0 to 2 (3 is excluded)
+# or
+d = np.array(range(0, 3))
+# or
+d = np.arange(15, 0, -1)    #just like range
 
 # precendence
 # string > int, float > int
 # if one element is a string and rest are int, all the elements will be converted to string
 
 a3: np.ndarray = np.array([7, 8, "9"])
+
+# accessing columns
+a1[:, 0]  # slice all and return the 1st column
+
+# height, weight
+players = [(170, 63), (180, 70), (185, 60), (200, 95), (160, 50)]
+skills = ["bat", "bat", "wk", "bowler", "AR"]
+
+# what is the height of person who is at 2nd index
+h = players[2][0]
+
+# all the players who are batsmen
+s = players[skills == "bat"]
